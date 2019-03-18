7d25e9152e8f27967fd538d2686d842ad307bac6
Huu Hoang <hoangduchuuvn@gmail.com>
2019-03-19 00:54:17 +0700

    [DEV] dialog function

--- app/src/main/java/com/khtn/androidcamp/MainActivity.kt
+++ app/src/main/java/com/khtn/androidcamp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.khtn.androidcamp
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,7 +26,34 @@ class MainActivity : AppCompatActivity() {
         btnLogin.setOnClickListener {
             // Khai báo listener khi click button
             Log.d("YourTagName", "Clicked Button Login")
+            showDialog()
         }
+
+
+    }
+
+    private fun showDialog() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("My Title")
+            .setMessage("My Long long long long messages")
+            .setPositiveButton("OK", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    // do Some thing
+                }
+
+            })
+            .setNegativeButton("Cancel", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    dialog?.dismiss()
+                }
+
+            })
+
+        val myDialog = builder.create();
+        myDialog.show()
+
+
     }
 }
 
