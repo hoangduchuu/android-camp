b2f205787465902be48f69bd487986a75511ece4
Huu Hoang <hoangduchuuvn@gmail.com>
2019-04-04 22:27:45 +0700

    pbloading with asyntask

--- app/src/main/java/com/khtn/androidcamp/MainActivity.kt
+++ app/src/main/java/com/khtn/androidcamp/MainActivity.kt
@@ -1,54 +1,53 @@
 package com.khtn.androidcamp
 
 import android.annotation.SuppressLint
-import android.app.Dialog
-import android.content.DialogInterface
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
-import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 @SuppressLint("SetTextI18n")
 
-
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var loading: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)   // Khai báo layout
+        loading = pbLoading
 
+        downloadSlide()
 
-        tvUserName.text = "Android KHTN"
-        btnLogin.text = "Register"
+    }
 
+    private fun downloadSlide() {
+        DownloadTask().execute()
+    }
 
-        btnLogin.setOnClickListener {
-            // Khai báo listener khi click button
-            Log.d("YourTagName", "Clicked Button Login")
-            showDialog()
+    inner class DownloadTask : AsyncTask<Void, Void, String>() {
+        override fun doInBackground(vararg params: Void?): String {
+            pbLoading.visibility = View.VISIBLE // show loading
+
+            Thread.sleep(3000) // clone our job in 3s
+            return ""
         }
 
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            // when finished
 
+            pbLoading.visibility = View.GONE // hide loading
+
+        }
     }
 
-    private fun showDialog() {
 
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("My Title")
-            .setMessage("My Long long long long messages")
-            .setPositiveButton("OK") { _, _ ->
-                // do Some thing
-            }
-            .setNegativeButton("Cancel"
-            ) { dialog, _ -> dialog?.dismiss() }
-
-        val myDialog = builder.create();
-        myDialog.show()
-
-
-    }
 }
+
+
 
 
 
