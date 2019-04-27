5024da5defb4507142cbbeef11c13fca2b35f0ef
Huu Hoang <hoangduchuuvn@gmail.com>
2019-04-27 18:27:49 +0700

    [DEV] Using OkHttp with Gson

--- app/src/main/java/com/khtn/androidcamp/MainActivity.kt
+++ app/src/main/java/com/khtn/androidcamp/MainActivity.kt
@@ -7,7 +7,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.View
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 @SuppressLint("SetTextI18n")
 
@@ -17,6 +21,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGetData.setOnClickListener {
+            getData(edtUserID.text.toString())
+        }
+    }
+
+    private fun getData(id: String) {
+        pbLoading.visibility = View.VISIBLE
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://jsonplaceholder.typicode.com/todos/$id")
+            .build()
+
+        client.newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    runOnUiThread {
+                        tvData.text = "ERROR: ${e.localizedMessage}"
+                        pbLoading.visibility = View.GONE
+
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val json = response.body()!!.string()
+                    val user = Gson().fromJson(json, User::class.java)
+                    runOnUiThread {
+                        tvData.text = user.toString()
+                        pbLoading.visibility = View.GONE
+                    }
+                }
+
+            })
     }
 }
 
---
+++ app/src/main/java/com/khtn/androidcamp/User.kt
@@ -0,0 +1,10 @@
+package com.khtn.androidcamp
+
+/**
+ * Created by Huu Hoang on 4/27/19.
+ */
+data class User(    val userId: Int? = null,
+                    val id: Int? = null,
+                    val title: String? = null,
+                    val complete : Boolean)
+
