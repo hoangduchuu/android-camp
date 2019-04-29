cc0b24f96ed749aaa992e49f0d1b2d9638b80764
Nam Pham Hoang <nam.phho@gmail.com>
2019-04-29 11:56:32 +0700

    [dev] added some log for more information

--- app/src/main/java/com/khtn/androidcamp/ArticleFragment.kt
+++ app/src/main/java/com/khtn/androidcamp/ArticleFragment.kt
@@ -1,7 +1,9 @@
 package com.khtn.androidcamp
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,8 +12,59 @@ import android.view.ViewGroup
  * Created by Huu Hoang on 4/25/19.
  */
 class ArticleFragment : Fragment() {
+    val TAG = ArticleFragment::class.java.simpleName
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        Log.e(TAG, "onAttached")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e(TAG, "onCreate")
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.e(TAG, "onCreateView")
         return inflater.inflate(R.layout.fragment_article, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.e(TAG, "onViewCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.e(TAG, "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.e(TAG, "onDetach")
     }
 }
