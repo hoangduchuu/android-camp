95d827d4add0a6ba5f5e7f7660d8bd40f0b814c2
Huu Hoang <hoangduchuuvn@gmail.com>
2019-05-16 18:09:10 +0700

    [DEV] implement empty map with API key, and MapCallBack

---
+++ app/src/main/java/com/khtn/androidcamp/MapsActivity.kt
@@ -0,0 +1,28 @@
+package com.khtn.androidcamp
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+@SuppressLint("SetTextI18n")
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    lateinit var mMap :GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.mMap = googleMap
+    }
+}
