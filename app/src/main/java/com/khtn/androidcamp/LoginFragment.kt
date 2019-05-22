361cc5a51a2165f68bebf181244ca8ffd34bba8f
Huu Hoang <hoangduchuuvn@gmail.com>
2019-05-22 16:37:11 +0700

    [DEV] finish login funciton

--- app/src/main/java/com/khtn/androidcamp/LoginFragment.kt
+++ app/src/main/java/com/khtn/androidcamp/LoginFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.login_fragment.*
 
 /**
@@ -19,6 +21,8 @@ class LoginFragment : BaseFragment() {
 
     lateinit var mListener: LoginFragment.Listener
 
+    lateinit var mAuth: FirebaseAuth
+
     override fun getTagName(): String {
         return LoginFragment::class.java.simpleName
     }
@@ -29,12 +33,37 @@ class LoginFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    private fun initViews() {
         tvRegister.setOnClickListener {
             mListener.openRegisterScreen()
         }
 
         tvReset.setOnClickListener {
             mListener.openForgotScreen()
+        }
+
+        btnLogin.setOnClickListener {
+            val email = edtmail.text.toString().trim()
+            val password = edPassword.text.toString().trim()
+            mAuth.signInWithEmailAndPassword(email,password)
+                .addOnFailureListener {
+                    Toast.makeText(context,"Login Failed: ${it.localizedMessage}", Toast.LENGTH_SHORT).show()
+                }.addOnSuccessListener {
+                    Toast.makeText(context,"Login success: ${it.user.email}", Toast.LENGTH_SHORT).show()
+                }.addOnCanceledListener {
+                    Toast.makeText(context,"Login Canceled ", Toast.LENGTH_SHORT).show()
+                }
+
         }
     }
 
